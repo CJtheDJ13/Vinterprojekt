3ec16554f6acbfff91a4b356e64b8bf6d67b1bfc
CJtheDJ <carljohanedstrom@gmail.com>
2025-12-17 10:07:19 +0100

    Karaktärer är nästan klara

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,19 +2,19 @@
 
 // Player Stats:
 int hp = 0;
+int maxhp = 0;
 int damageMax = 0;
 int damageMin = 0;
 string name = "";
 
-// Knight:
-bool block;
 // Lancer:
-bool moreDMG;
-// Titan:
-bool miss;
+float moreDMG;
 
 // Enemies:
-int test = 500;
+int testHP = 1000;
+int EminDMG = 100;
+int EmaxDMG = 500;
+
 
 Console.WriteLine("""
 Choose Your Chacter:
@@ -24,7 +24,7 @@ Thread.Sleep(500);
 
 Console.WriteLine("""
 Knight (1)
-Weapon = Warhammer (125 - 175 Damage)
+Weapon = Sword and Shield (125 - 175 Damage)
 Ability: 
 HP = 1000
 Trait: 20% Chance to Completely Block the Enemy's Attack
@@ -37,7 +37,7 @@ Trait: 35% Chance to do 80% More Damage
 
 Titan (3)
 Weapon = Huge Sword (225 - 300 Damage)
-Ability: 
+Ability: Ground Slam
 HP 3000
 Trait: 25% Chance to Miss Your Attack Completely
 
@@ -47,7 +47,7 @@ Trait: 25% Chance to Miss Your Attack Completely
 string answer = Console.ReadLine();
 int result = 0;
 bool isNumber = int.TryParse(answer, out result);
-while(isNumber != true || result > 3 || result < 1)
+while (isNumber != true || result > 3 || result < 1)
 {
     Console.ForegroundColor = ConsoleColor.DarkYellow;
     Console.WriteLine("Write a number between 1 and 3!");
@@ -59,6 +59,7 @@ while(isNumber != true || result > 3 || result < 1)
 if (result == 1)
 {
     name = "Knight";
+    maxhp = 1000;
     hp = 1000;
     damageMax = 175;
     damageMin = 125;
@@ -71,6 +72,7 @@ if (result == 1)
 else if (result == 2)
 {
     name = "Lancer";
+    maxhp = 750;
     hp = 750;
     damageMax = 125;
     damageMin = 90;
@@ -83,6 +85,7 @@ else if (result == 2)
 else if (result == 3)
 {
     name = "Titan";
+    maxhp = 3000;
     hp = 3000;
     damageMax = 300;
     damageMin = 225;
@@ -102,10 +105,62 @@ To head into the Arena,
 Console.ForegroundColor = ConsoleColor.Green;
 Console.WriteLine("Press: Enter");
 Console.ResetColor();
+Console.ReadLine();
 
 
 // TESTA SÅ ATT DMG OCH HP OCH ABILITIES FUNKAR!!!
+Random trL = new Random();
+Random trT = new Random();
+moreDMG = 1.8f;
 
 
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"Enemy HP = {testHP}");
+    Console.WriteLine("Press ENTER to Damage");
+    Console.ReadLine();
 
-Console.ReadLine();
+    bool trLbool = trL.NextDouble() < 0.35;
+    bool trTbool = trT.NextDouble() < 0.25;
+    if (name == "Lancer")
+    {
+        if (trLbool == true)
+        {
+            testHP -= (int) MathF.Round(Random.Shared.Next(damageMin, damageMax) * moreDMG);
+            Console.WriteLine("Ability Active!");
+        }
+        else
+        {
+            testHP -= Random.Shared.Next(damageMin, damageMax);
+        }
+    }
+    if (name == "Titan")
+    {
+        if (trTbool == true)
+        {
+            testHP -= 0;
+            Console.WriteLine("Miss!");
+        }
+        else
+        {
+            testHP -= Random.Shared.Next(damageMin, damageMax);
+        }
+    }
+
+    hp -= Random.Shared.Next(EminDMG, EmaxDMG);
+
+    Console.WriteLine($"Enemy HP = {testHP}");
+    testHP = 1000 - testHP;
+    Console.WriteLine($"You did {testHP} Damage");
+    testHP = 1000;
+    Console.WriteLine($"Your HP = {hp}");
+    // maxhp -= hp;
+    Console.WriteLine($"Enemy did {}");
+
+    Console.ReadLine();
+}
+
+// FIXA SÅ ATT TEST KAN GÖRA SKADA PÅ SPELAREN
+// FIXA KNIGHT TRAIT
+
